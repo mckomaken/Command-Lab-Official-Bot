03364f7f1014ea2996f90b871ec164994fa86906
Syunngiku0402 <haruokcxj01524@docomo.ne.jp>
2024-04-09 06:03:44 +0900

    cpingコマンド上限設定

--- cogs/chelp.py
+++ cogs/chelp.py
@@ -48,10 +48,13 @@ class CHelpCog(commands.Cog):
     @app_commands.command(name="cping", description="pingを計測します")
     @app_commands.guild_only()
     @app_commands.describe(
-        count="【初期値(未記入) : 1】実行回数を自然数で入力してください。",
+        count="【初期値(未記入) : 1】実行回数を10以下の自然数で入力してください。",
         t_or_f="【初期値(未記入) : True】True : 3分おきに実行 ・ False : 直ぐ(１秒おき)に実行",
     )
     async def cping(self, interaction: discord.Interaction, count: Optional[int] = 1, t_or_f: Optional[bool] = True):
+
+        if count < 10:
+            return
 
         pi1JST_time = datetime.now()
         text1 = f'{round(self.bot.latency * 20)}tick\n{round(self.bot.latency * 1000)}ms'
@@ -101,8 +104,11 @@ class CHelpCog(commands.Cog):
                         await interaction.user.send(embed=ping3_embed)
 
                     interval_cb.start()
+
+        elif count > 10:
+            await interaction.response.send_message("countには10以下の自然数を入れてね(^^♪\n自然数がわからない人はこのサーバーから追放するね(^^♪♪♪", ephemeral=True)
         else:
-            await interaction.response.send_message("countには自然数を入れてね(^^♪\n自然数がわからない人はこのサーバーから追放するね(^^♪♪♪", ephemeral=True)
+            await interaction.response.send_message("countには10以下の自然数を入れてね(^^♪\n自然数がわからない人はこのサーバーから追放するね(^^♪♪♪", ephemeral=True)
 
 
 async def setup(bot: commands.Bot):
