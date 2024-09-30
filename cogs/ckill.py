e6c445e9a83a4b530188e40d6bb94e20f67e0519
ap12-code <ap12@ap12.net>
2024-09-30 15:52:04 +0900

    fix: str to display name

--- cogs/ckill.py
+++ cogs/ckill.py
@@ -1,3 +1,4 @@
+from typing import Optional
 import discord
 from discord import Member, app_commands
 from discord.ext import commands
@@ -6,34 +7,38 @@ import json
 import random
 
 
-def kill(name, target=None):
-
-    # jsonファイル を dict(lang_data) に変換
-    with open(file='./assets/ja_jp.json', mode='r', encoding='utf-8') as file:
-        lang_data = json.load(file)
-
-    # lang_data からデスログ、エンティティ名、アイテム名を抽出
-    death_logs = [v for k, v in lang_data.items() if "death." in k]
-    entities = [v.replace("のスポーンエッグ", "") for k, v in lang_data.items() if "item.minecraft." in k and "_spawn_egg" in k]
-    items = [v for k, v in lang_data.items() if "item.minecraft." in k]
-
-    # もしtargetがいなかったら、targetをエンティティの中から選出する
-    if target is None:
-        target = random.choice(entities)
-
-    # キルログ生成
-    death_log = random.choice(death_logs).replace("%1$s", name + " ").replace("%2$s", target).replace("%3$s", f"[{random.choice(items)}]")
-    return death_log
-
-
 class CKill(commands.Cog):
+    lang_data: dict[str, str]
+
     def __init__(self, bot: commands.Bot):
         self.bot = bot
 
     @app_commands.command(name="ckill", description="キルコマンド(ネタ)")
     @app_commands.describe(target="キルするユーザー(任意)")
-    async def ckill(self, interaction: discord.Interaction, target: str = None):
-        await interaction.response.send_message(kill(interaction.user.mention, target))
+    async def ckill(self, interaction: discord.Interaction, target: Optional[Member] = None):
+        # もしtargetがいなかったら、targetをエンティティの中から選出する
+        if target is None:
+            target = random.choice(self.entities)
+
+        # キルログ生成
+        death_log = random.choice(self.death_logs) \
+            .replace("%1$s", interaction.user.display_name + " ") \
+            .replace("%2$s", target) \
+            .replace("%3$s", f"[{random.choice(self.items)}]")
+
+        await interaction.response.send_message(death_log)
+
+    async def cog_load(self) -> None:
+        # jsonファイル を dict(lang_data) に変換
+        with open(file='./assets/ja_jp.json', mode='r', encoding='utf-8') as file:
+            self.lang_data = json.load(file)
+
+            self.death_logs = [v for k, v in self.lang_data.items() if "death." in k]
+            self.entities = [
+                v.replace("のスポーンエッグ", "") for k, v in self.lang_data.items()
+                if "item.minecraft." in k and "_spawn_egg" in k
+            ]
+            self.items = [v for k, v in self.lang_data.items() if "item.minecraft." in k]
 
 
 async def setup(bot: commands.Bot):
