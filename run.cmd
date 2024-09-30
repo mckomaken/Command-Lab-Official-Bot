26df5c12e289c80d4f17b07cf92b436f8e456e5e
ap12 <ap12@ap12.net>
2024-09-30 18:25:26 +0900

    fix: run file

--- run.cmd
+++ run.cmd
@@ -1,26 +1,14 @@
 @echo off
 cd %~dp0
+setlocal
 
-set PY="python"
+set PY="py"
 
-if exist ".\_StartBot" (
-    mkdir tmp
-    copy .\_StartBot\tmp\bump_data.json .\tmp\
-    rd /S /Q .\_StartBot
-)
-
-git clone git@github.com:Syunngiku0402/Command-Lab-Official-Bot _StartBot
-cd .\_StartBot
-rd /S /Q .git
-
-
+git pull
 %PY% -m pip install -r requirements.txt
-copy /Y ..\config.json .\config\config.json
-copy /Y ..\CMTK.txt .\CMTK.txt
-mkdir tmp
 
-copy ..\tmp\bump_data.json .\tmp\
-rd /S /Q ..\tmp
-
+set /p TOKEN=<..\CMTK.txt
 %PY% CommandLab.py
-exit
+
+endlocal
+pause
