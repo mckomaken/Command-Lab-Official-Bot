5c596d841bf8146baaf1acc9a8722869005af170
Syunngiku0402 <86222854+Syunngiku0402@users.noreply.github.com>
2024-11-03 19:16:34 +0900

    MEE6レベルアップ、自動送信機能追加v5

--- cogs/cmee6level.py
+++ cogs/cmee6level.py
@@ -24,7 +24,8 @@ class CMee6level(commands.Cog):
                 else:
                     text = ""
                 mee6_channel = await self.bot.fetch_channel(config.mee6.levelup)  # 新たに作るmee6通知チャンネル
-                if message.author.roles not in [config.mee6.levelupnoticeoff]:  # mee6levelup無効化ロールを持っているかどうか
+                levelupnoticeoff = await message.guild.fetch_role(config.mee6.levelupnoticeoff)
+                if levelupnoticeoff not in message.author.roles: # mee6levelup無効化ロールを持っているかどうか
                     await mee6_channel.send(f"{text}/xp reached <@{userid}> level {level}")
                 else:
                     await mee6_channel.send(f"{text}/xp reached {username} level {level}")
