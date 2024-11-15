cde6f8cc832fb0cdfa58fe25facd3a4108bbff0c
Syunngiku0402 <haruokcxj01524@docomo.ne.jp>
2024-11-16 01:30:02 +0900

    進数変換コマンド実装v2

--- cogs/cradix.py
+++ cogs/cradix.py
@@ -13,15 +13,16 @@ class CRadix(commands.Cog):
     async def cybase64(self, interaction: discord.Interaction, num: int):
         absnum = abs(num)
         DESCRIPTION = f"""
+```
 |入力値| = {absnum}
-2進数 : {np.base_repr(absnum, 2)}
-3進数 : {np.base_repr(absnum, 3)}
-4進数 : {np.base_repr(absnum, 4)}
-5進数 : {np.base_repr(absnum, 5)}
-6進数 : {np.base_repr(absnum, 6)}
-7進数 : {np.base_repr(absnum, 7)}
-8進数 : {np.base_repr(absnum, 8)}
-9進数 : {np.base_repr(absnum, 9)}
+2進数  : {np.base_repr(absnum, 2)}
+3進数  : {np.base_repr(absnum, 3)}
+4進数  : {np.base_repr(absnum, 4)}
+5進数  : {np.base_repr(absnum, 5)}
+6進数  : {np.base_repr(absnum, 6)}
+7進数  : {np.base_repr(absnum, 7)}
+8進数  : {np.base_repr(absnum, 8)}
+9進数  : {np.base_repr(absnum, 9)}
 10進数 : {np.base_repr(absnum, 10)}
 11進数 : {np.base_repr(absnum, 11)}
 12進数 : {np.base_repr(absnum, 12)}
@@ -29,6 +30,7 @@ class CRadix(commands.Cog):
 14進数 : {np.base_repr(absnum, 14)}
 15進数 : {np.base_repr(absnum, 15)}
 16進数 : {np.base_repr(absnum, 16)}
+```
 """
         radix_embed = discord.Embed(
             title="進数変換",
