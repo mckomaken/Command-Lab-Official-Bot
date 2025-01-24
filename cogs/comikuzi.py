6e8477b1ac232df8829f234b5225733c1669f40b
Syunngiku0402 <haruokcxj01524@docomo.ne.jp>
2025-01-24 22:58:20 +0900

    おみくじコマンド実装v4

--- cogs/comikuzi.py
+++ cogs/comikuzi.py
@@ -14,30 +14,35 @@ async def omikuzi1(interaction: Interaction):
             description="おみくじ結果\n# 大吉",
             color=0xffff00
         )
+        om1_1_embed.set_author(name=interaction.user.display_name, icon_url=interaction.user.avatar.url)
         await interaction.response.send_message(embed=om1_1_embed)
     elif num >= 730:
         om1_2_embed = discord.Embed(
             description="おみくじ結果\n# 吉",
             color=0xffff00
         )
+        om1_2_embed.set_author(name=interaction.user.display_name, icon_url=interaction.user.avatar.url)
         await interaction.response.send_message(embed=om1_2_embed)
     elif num >= 380:
         om1_3_embed = discord.Embed(
             description="おみくじ結果\n# 中吉",
             color=0xffff00
         )
+        om1_3_embed.set_author(name=interaction.user.display_name, icon_url=interaction.user.avatar.url)
         await interaction.response.send_message(embed=om1_3_embed)
     elif num >= 260:
         om1_4_embed = discord.Embed(
             description="おみくじ結果\n# 小吉",
             color=0xffff00
         )
+        om1_4_embed.set_author(name=interaction.user.display_name, icon_url=interaction.user.avatar.url)
         await interaction.response.send_message(embed=om1_4_embed)
     else:
         om1_5_embed = discord.Embed(
             description="おみくじ結果\n# 末吉",
             color=0xffff00
         )
+        om1_5_embed.set_author(name=interaction.user.display_name, icon_url=interaction.user.avatar.url)
         await interaction.response.send_message(embed=om1_5_embed)
 
 
@@ -48,42 +53,49 @@ async def omikuzi2(interaction: Interaction):
             description="おみくじ結果\n# 大吉",
             color=0x00ffff
         )
+        om2_1_embed.set_author(name=interaction.user.display_name, icon_url=interaction.user.avatar.url)
         await interaction.response.send_message(embed=om2_1_embed)
     elif num >= 790:
         om2_2_embed = discord.Embed(
             description="おみくじ結果\n# 吉",
             color=0x00ffff
         )
+        om2_2_embed.set_author(name=interaction.user.display_name, icon_url=interaction.user.avatar.url)
         await interaction.response.send_message(embed=om2_2_embed)
     elif num >= 470:
         om2_3_embed = discord.Embed(
             description="おみくじ結果\n# 中吉",
             color=0x00ffff
         )
+        om2_3_embed.set_author(name=interaction.user.display_name, icon_url=interaction.user.avatar.url)
         await interaction.response.send_message(embed=om2_3_embed)
     elif num >= 380:
         om2_4_embed = discord.Embed(
             description="おみくじ結果\n# 小吉",
             color=0x00ffff
         )
+        om2_4_embed.set_author(name=interaction.user.display_name, icon_url=interaction.user.avatar.url)
         await interaction.response.send_message(embed=om2_4_embed)
     elif num >= 150:
         om2_5_embed = discord.Embed(
             description="おみくじ結果\n# 末吉",
             color=0x00ffff
         )
+        om2_5_embed.set_author(name=interaction.user.display_name, icon_url=interaction.user.avatar.url)
         await interaction.response.send_message(embed=om2_5_embed)
     elif num >= 50:
         om2_6_embed = discord.Embed(
             description="おみくじ結果\n# 半凶",
             color=0x00ffff
         )
+        om2_6_embed.set_author(name=interaction.user.display_name, icon_url=interaction.user.avatar.url)
         await interaction.response.send_message(embed=om2_6_embed)
     else:
         om2_7_embed = discord.Embed(
             description="おみくじ結果\n# 凶",
             color=0x00ffff
         )
+        om2_7_embed.set_author(name=interaction.user.display_name, icon_url=interaction.user.avatar.url)
         await interaction.response.send_message(embed=om2_7_embed)
 
 
@@ -100,9 +112,30 @@ class COmikuzi(commands.Cog):
             app_commands.Choice(name="オリジナルバージョン", value="om3")
         ]
     )
-    async def cpresent(self, interaction: discord.Interaction, choice: Optional[str] = None, help: Optional[bool] = False):
+    async def comikuzi(self, interaction: discord.Interaction, choice: Optional[str] = None, help: Optional[bool] = False):
+        OMHELP1 = """
+# 遊び方
+`/comikuzi` : 3つのどれかのおみくじが選ばれ、吉凶が決まります
+`/comikuzi choice:○○○バージョン` : 選んだおみくじから吉凶が決まります
+`/comikuzi help:True` : このEmbedが表示されます
+# 確率
+太宰府天満宮バージョン
+```　大吉＞　　吉＞　中吉＞　小吉＞　末吉
+１０％＞１７％＞３５％＞１２％＞２６％```
+
+北野天満宮バージョン
+```大吉＞　　吉＞　中吉＞小吉＞　末吉＞　半凶＞　凶
+７％＞１４％＞３２％＞９％＞２３％＞１０％＞５％```
+
+オリジナルバージョン
+```まだない(笑)```
+"""
         if help is True:  # help用のエンベッド表示
-            await interaction.response.send_message("送信しました", ephemeral=True)
+            omhelp1_embed = discord.Embed(
+                title="各おみくじの排出確率＆遊び方",
+                description=OMHELP1
+            )
+            await interaction.response.send_message(embed=omhelp1_embed, ephemeral=True)
         else:  # おみくじ
             choosenum = random.randint(1, 3)
             if choice == "om1":
