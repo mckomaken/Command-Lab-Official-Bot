da76fece8fa7d57cea72c090d31788875afc2179
Syunngiku0402 <haruokcxj01524@docomo.ne.jp>
2025-05-01 01:06:49 +0900

    ロール不足のお知らせ送信チャンネル追加

--- cogs/crole_notice.py
+++ cogs/crole_notice.py
@@ -23,6 +23,7 @@ class Rolenotice(commands.Cog):
         pc = message.guild.get_role(config.roles.personalcomputer)
         sumaho = message.guild.get_role(config.roles.smartphone)
         gameki = message.guild.get_role(config.roles.gamemachine)
+        role_ch = await self.bot.fetch_channel(config.role_set_ch)
 
         comlank = ""
         jebe = ""
@@ -40,6 +41,7 @@ class Rolenotice(commands.Cog):
             send = True
         if send is True:
             await message.reply(f"以下のロールが付いていません\n{comlank}{jebe}{kisyu}<#{config.role_set_ch}>でロールを付けてきてください\n-# なおこのメッセージは10秒後に消えます", silent=True, delete_after=10, allowed_mentions=discord.AllowedMentions(roles=False))
+            await role_ch.send(f"{message.author.mention}\nこちらのチャンネルの上部で\n{comlank}{jebe}{kisyu}対応するボタンを押して、ロールを付けてきてください\n-# なおこのメッセージは20秒後に消えます", silent=True, delete_after=20, allowed_mentions=discord.AllowedMentions(roles=False))
         else:
             return
 
