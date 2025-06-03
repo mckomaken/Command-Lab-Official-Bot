ee7994893fe17e48754f0eaf5763f99fbb0e8438
Syunngiku0402 <haruokcxj01524@docomo.ne.jp>
2025-06-04 02:04:33 +0900

    コマ研レベル通知v2

--- cogs/cmee6level.py
+++ cogs/cmee6level.py
@@ -11,12 +11,11 @@ class CMee6level(commands.Cog):
 
     @commands.Cog.listener("on_message")
     async def on_message(self, message: discord.Message):
-        if "mee6level" in message.content:
+        if message.content.startswith("mee6lecel"):
             if message.channel.id in [config.mee6.botch]:  # 非公開mee6-level通知チャンネル
                 userid = int(message.content.split(",")[1])  # userid表示
                 username = str(message.content.split(",")[2])  # user名表示
                 level = int(message.content.split(",")[3])  # レベル
-                print(f"id:{userid}, name:{username}, lv:{level}")
                 if (level % 50 == 0):
                     text = "# "
                 elif (level % 10 == 0):
@@ -35,7 +34,7 @@ class CMee6level(commands.Cog):
                 jointime1day = lvupuser.joined_at + timedelta(days=1)
 
                 if level == 1 and levelupnoticeoff not in lvupuser.roles:
-                    await mee6_channel.send(f"{text}/xp reached <@{userid}> level {level}\n-# メンション通知がうるさいと感じたら<#892255648295841842>で`MEE6レベル無効化`ロールを付けてね")
+                    await mee6_channel.send(f"{text}/xp reached <@{userid}> mee6-level {level}\n-# メンション通知がうるさいと感じたら<#892255648295841842>で`MEE6レベル無効化`ロールを付けてね")
                     return
                 elif level >= 5:
                     if senndennkenn not in lvupuser.roles and hanabira not in lvupuser.roles:
@@ -44,10 +43,32 @@ class CMee6level(commands.Cog):
                         else:
                             await admin_channel.send(f"<@{userid}>：宣伝権(仮)ロールを{jointime1day}に付与してください")
                 userdisp = f"`{username}`" if levelupnoticeoff in lvupuser.roles else f"<@{userid}>"
-                await mee6_channel.send(f"{text}/xp reached {userdisp} level {level}")
+                await mee6_channel.send(f"{text}/xp reached {userdisp} mee6-level {level}")
                 userdb = session.query(User).filter_by(userid=userid).first()
                 userdb.mee6level = level
                 session.commit()
+        elif message.content.startswith("mcmdlevel"):
+            if message.channel.id in [config.mee6.botch]:  # 非公開mee6-level通知チャンネル
+                userid = int(message.content.split(",")[1])  # userid表示
+                username = str(message.content.split(",")[2])  # user名表示
+                level = int(message.content.split(",")[3])  # レベル
+                if (level % 500 == 0):
+                    text = "# "
+                elif (level % 100 == 0):
+                    text = "## "
+                elif (level % 50 == 0):
+                    text = "### "
+                elif (level % 10 == 0):
+                    text = ""
+                else:
+                    return
+                mee6_channel = await self.bot.fetch_channel(config.mee6.levelup)  # 新たに作るmee6通知チャンネル
+                levelupnoticeoff = message.guild.get_role(config.mee6.levelupnoticeoff)
+                senndennkenn = message.guild.get_role(config.mee6.senndennkenn)
+                hanabira = message.guild.get_role(config.mee6.hanabira)
+                lvupuser = await message.guild.fetch_member(userid)
+                userdisp = f"`{username}`" if levelupnoticeoff in lvupuser.roles else f"<@{userid}>"
+                await mee6_channel.send(f"{text}/xp reached {userdisp} mcmd-level {level}")
 
 
 async def setup(bot: commands.Bot):
