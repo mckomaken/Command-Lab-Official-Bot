30dae841423c3e3a32af35d89ce5704d6e851ded
Syunngiku0402 <haruokcxj01524@docomo.ne.jp>
2025-06-18 03:24:59 +0900

    .(もう打つのめんどくなった)

--- cogs/cnews.py
+++ cogs/cnews.py
@@ -29,7 +29,8 @@ class CNews(commands.Cog):
             async with aiohttp.ClientSession(JAVA_PATCH_NOTES) as client:
                 async with client.get("") as resp:
                     data = PatchNote.model_validate(await resp.json(content_type=None, encoding="utf-8-sig"))
-                    for entry in data.entries["version"]:
+                    for entry in data.entries:
+                        print(entry)
                         if entry.version == version:
                             embed = discord.Embed(
                                 title=entry.title,
