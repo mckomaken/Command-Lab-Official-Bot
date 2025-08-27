bb7ffe1603c1b168f8e5a2a1c63420eb6ccb06d4
Syunngiku0402 <haruokcxj01524@docomo.ne.jp>
2025-08-28 02:51:39 +0900

    fix: リンクコピー機能実装v3

--- cogs/cybase64.py
+++ cogs/cybase64.py
@@ -25,7 +25,7 @@ class UrlView(discord.ui.View):
             ephemeral=True,
         )
 
-    @discord.ui.button(label="リンクをコピー", style=discord.ButtonStyle.link, custom_id="ydancopy")
+    @discord.ui.button(label="リンクをコピー", style=discord.ButtonStyle.gray, custom_id="ydancopy")
     async def copy_url(self, interaction: discord.Interaction, item: discord.ui.Item):
         await pyperclip.copy(self.url)
         await interaction.response.send_message("クリップボードにコピーしました！", ephemeral=True)
