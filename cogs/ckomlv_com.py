a932b9233b534323867b0e90caf4de5b78f00cd2
Syunngiku0402 <haruokcxj01524@docomo.ne.jp>
2026-08-12 19:42:27 +0900

    fix: 計算式削除

--- cogs/ckomlv_com.py
+++ cogs/ckomlv_com.py
@@ -64,7 +64,7 @@ class Cmdbotlevelcom(commands.Cog):
                     level_embed.add_field(name="有効チャット数", value=f"```go\n{userdb.chatcount} チャット\n```", inline=True)
                     level_embed.add_field(name="MEE6レベル", value=f"```go\n{userdb.mee6level} Level\n```", inline=True)
                     level_embed.add_field(name="デイリーログイン日数", value=f"```go\n{userdb.dailylogincount} 日\n```", inline=True)
-                    level_embed.add_field(name="明日のデイリー獲得予定経験値量", value=f"```go\n1 + ({userdb.level} / 400) ^ 0.33\n≒ {tomorrow_daily_exp} exp\n```", inline=True)
+                    level_embed.add_field(name="明日のデイリー獲得予定経験値量", value=f"```go\n{tomorrow_daily_exp} exp\n```", inline=True)
                 await interaction.response.send_message(embed=level_embed, silent=True)
                 userdb.allexp = (userdb.level * 10000) + userdb.exp
                 session.commit()
@@ -256,7 +256,7 @@ class Cmdbotlevelcom(commands.Cog):
                 level_embed.add_field(name="有効チャット数", value=f"```go\n{setuserdb.chatcount} チャット\n```", inline=True)
                 level_embed.add_field(name="MEE6レベル", value=f"```go\n{setuserdb.mee6level} Level\n```", inline=True)
                 level_embed.add_field(name="デイリーログイン日数", value=f"```go\n{setuserdb.dailylogincount} 日\n```", inline=True)
-                level_embed.add_field(name="明日のデイリー獲得予定経験値量", value=f"```go\n1 + ({setuserdb.level} / 400) ^ 0.33\n≒ {tomorrow_daily_exp} exp\n```", inline=True)
+                level_embed.add_field(name="明日のデイリー獲得予定経験値量", value=f"```go\n{tomorrow_daily_exp} exp\n```", inline=True)
                 await interaction.response.send_message(embed=level_embed, silent=True, allowed_mentions=discord.AllowedMentions.none())
 
 
